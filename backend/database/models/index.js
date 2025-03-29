372b708fe2228b460884a22bcde6688c6a02c383
Ricardo <rihozmar@gmail.com>
2025-03-29 18:17:27 +0100

    Relaciones de tablas

--- backend/database/models/index.js
+++ backend/database/models/index.js
@@ -1,9 +1,43 @@
 const { Sequelize } = require('sequelize');
 const sequelize = require('./../../config/db');
 
+//Importamos los modelos
+const Deportes=require('./Deportes')
+const Eventos=require('./Eventos')
+const Federados=require('./Federados')
+const Gimnasios=require('./Gimnasios')
+const Imagenes=require('./Imagenes')
+const Licencias=require('./Licencias')
+const Usuarios_Eventos=require('./Usuarios_Eventos')
+const Usuarios=require('./Usuarios')
 
 
+//Relaciones de usuario
+Gimnasios.hasOne(Usuarios,{foreignKey:'id_academia'})
+Usuarios.hasMany(Imagenes,{foreignKey:'id_usuario'})
+
+
+//Relaciones de Deportes
+Deportes.hasOne(Eventos,{foreignKey:'deporte_id'})
+
+//Usuarios_registrado_evento
+Usuarios.belongsToMany(Eventos,{through:'Usuarios_registrado_evento',foreignKey:'id_usuario'})
+Eventos.belongsToMany(Usuarios,{through:'Usuarios_registrado_evento',foreignKey:'id_evento'})
+
+//Federados
+Usuarios.belongsToMany(Licencias,{through:'Federados',foreignKey:'id_usuario'})
+Licencias.belongsToMany(Usuarios,{through:'Federados',foreignKey:'id_licencia'})
+
+//Relaciones federaciones
+Deportes.hasOne(Licencias,{foreignKey:'id_deporte'})
 module.exports = {
     sequelize,
-   
+    Deportes,
+    Eventos,
+    Federados,
+    Gimnasios,
+    Imagenes,
+    Licencias,
+    Usuarios_Eventos,
+    Usuarios,
   };
