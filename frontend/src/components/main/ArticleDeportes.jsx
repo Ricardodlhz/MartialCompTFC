793916d783a0a728913660d2747d06856c594763
Ricardo <rihozmar@gmail.com>
2025-03-31 16:45:33 +0200

    Articles cambiados usando acerternity

--- frontend/src/components/main/ArticleDeportes.jsx
+++ frontend/src/components/main/ArticleDeportes.jsx
@@ -1,38 +1,202 @@
 import React from 'react'
+"use client";
+import { cn } from "./../../lib/utils";
 
 
 const ArticleDeportes = () => {
     return (
         <>
-            <article className='flex-[1_1_300px] h-[300px] bg-[url(./src/assets/deportes/judo.avif)] bg-cover bg-center'>
-             <p className="text">Judo</p>
+
+            <article className="flex-[1_1_300px] ">
+                <div
+                    className={cn(
+                        "group w-full cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl mx-auto flex flex-col justify-end p-4 border border-transparent dark:border-neutral-800",
+                        "bg-[url(./src/assets/deportes/judo.avif)] bg-cover bg-center",
+                        // Preload hover image by setting it in a pseudo-element
+                        "before:bg-[url(./src/assets/deportes/judogif.webp)] before:fixed before:inset-0 before:opacity-0 before:z-[-1]",
+                        "hover:bg-[url(./src/assets/deportes/judogif.webp)]",
+                        "hover:after:content-[''] hover:after:absolute hover:after:inset-0 hover:after:bg-black hover:after:opacity-50",
+                        "transition-all duration-500"
+                    )}
+                >
+                    <div className="text relative z-50">
+                        <h1 className="font-bold text-xl md:text-3xl text-gray-50 relative">
+                            Background Overlays
+                        </h1>
+                        <p className="font-normal text-base text-gray-50 relative my-4">
+                            This card is for some special elements, like displaying background
+                            gifs on hover only.
+                        </p>
+                    </div>
+                </div>
             </article>
 
-            <article className='flex-[1_1_300px] h-[300px]  bg-[url(./src/assets/deportes/bjj.jpg)] bg-cover bg-center '>
-             <p className="text">BJJ</p>  
+            <article className="flex-[1_1_300px] ">
+                <div
+                    className={cn(
+                        "group w-full cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl mx-auto flex flex-col justify-end p-4 border border-transparent dark:border-neutral-800",
+                        "bg-[url(./src/assets/deportes/bjj.jpg)] bg-cover",
+                        // Preload hover image by setting it in a pseudo-element
+                        "before:bg-[url(./src/assets/deportes/bjjgif.webp)] before:fixed before:inset-0 before:opacity-0 before:z-[-1]",
+                        "hover:bg-[url(./src/assets/deportes/bjjgif.webp)]",
+                        "hover:after:content-[''] hover:after:absolute hover:after:inset-0 hover:after:bg-black hover:after:opacity-50",
+                        "transition-all duration-500"
+                    )}
+                >
+                    <div className="text relative z-50">
+                        <h1 className="font-bold text-xl md:text-3xl text-gray-50 relative">
+                            Background Overlays
+                        </h1>
+                        <p className="font-normal text-base text-gray-50 relative my-4">
+                            This card is for some special elements, like displaying background
+                            gifs on hover only.
+                        </p>
+                    </div>
+                </div>
             </article>
-            <article className='flex-[1_1_300px] h-[300px]  bg-[url(./src/assets/deportes/boxeo.avif)] bg-cover bg-center '>
-             <p className="text">Boxeo</p>
+            <article className="flex-[1_1_300px] ">
+                <div
+                    className={cn(
+                        "group w-full cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl mx-auto flex flex-col justify-end p-4 border border-transparent dark:border-neutral-800",
+                        "bg-[url(./src/assets/deportes/boxeo.avif)] bg-cover bg-center",
+                        // Preload hover image by setting it in a pseudo-element
+                        "before:bg-[url(./src/assets/deportes/boxeogif.webp)] before:fixed before:inset-0 before:opacity-0 before:z-[-1]",
+                        "hover:bg-[url(./src/assets/deportes/boxeogif.webp)]",
+                        "hover:after:content-[''] hover:after:absolute hover:after:inset-0 hover:after:bg-black hover:after:opacity-50",
+                        "transition-all duration-500"
+                    )}
+                >
+                    <div className="text relative z-50">
+                        <h1 className="font-bold text-xl md:text-3xl text-gray-50 relative">
+                            Background Overlays
+                        </h1>
+                        <p className="font-normal text-base text-gray-50 relative my-4">
+                            This card is for some special elements, like displaying background
+                            gifs on hover only.
+                        </p>
+                    </div>
+                </div>
+            </article>
+            <article className="flex-[1_1_300px] ">
+                <div
+                    className={cn(
+                        "group w-full cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl mx-auto flex flex-col justify-end p-4 border border-transparent dark:border-neutral-800",
+                        "bg-[url(./src/assets/deportes/karate.avif)] bg-cover bg-center",
+                        // Preload hover image by setting it in a pseudo-element
+                        "before:bg-[url(./src/assets/deportes/karategif.webp)] before:fixed before:inset-0 before:opacity-0 before:z-[-1]",
+                        "hover:bg-[url(./src/assets/deportes/karategif.webp)]",
+                        "hover:after:content-[''] hover:after:absolute hover:after:inset-0 hover:after:bg-black hover:after:opacity-50",
+                        "transition-all duration-500"
+                    )}
+                >
+                    <div className="text relative z-50">
+                        <h1 className="font-bold text-xl md:text-3xl text-gray-50 relative">
+                            Background Overlays
+                        </h1>
+                        <p className="font-normal text-base text-gray-50 relative my-4">
+                            This card is for some special elements, like displaying background
+                            gifs on hover only.
+                        </p>
+                    </div>
+                </div>
             </article>
 
-            <article className='flex-[1_1_300px] h-[300px]  bg-[url(./src/assets/deportes/karate.avif)] bg-cover bg-center '>
-             <p className="text">Karate</p>  
-            </article>
-            <article className='flex-[1_1_300px] h-[300px]  bg-[url(./src/assets/deportes/lucha.avif)] bg-cover bg-center '>
-             <p className="text">Wrestling</p> 
+            <article className="flex-[1_1_300px] ">
+                <div
+                    className={cn(
+                        "group w-full cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl mx-auto flex flex-col justify-end p-4 border border-transparent dark:border-neutral-800",
+                        "bg-[url(./src/assets/deportes/lucha.avif)] bg-cover bg-center",
+                        // Preload hover image by setting it in a pseudo-element
+                        "before:bg-[url(./src/assets/deportes/luchagif.gif)] before:fixed before:inset-0 before:opacity-0 before:z-[-1]",
+                        "hover:bg-[url(./src/assets/deportes/luchagif.gif)]",
+                        "hover:after:content-[''] hover:after:absolute hover:after:inset-0 hover:after:bg-black hover:after:opacity-50",
+                        "transition-all duration-500"
+                    )}
+                >
+                    <div className="text relative z-50">
+                        <h1 className="font-bold text-xl md:text-3xl text-gray-50 relative">
+                            Background Overlays
+                        </h1>
+                        <p className="font-normal text-base text-gray-50 relative my-4">
+                            This card is for some special elements, like displaying background
+                            gifs on hover only.
+                        </p>
+                    </div>
+                </div>
             </article>
 
-            <article className='flex-[1_1_300px] h-[300px]  bg-[url(./src/assets/deportes/sambo.webp)] bg-cover bg-center '>
-             <p className="text">Sambo</p> 
+            <article className="flex-[1_1_300px] ">
+                <div
+                    className={cn(
+                        "group w-full cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl mx-auto flex flex-col justify-end p-4 border border-transparent dark:border-neutral-800",
+                        "bg-[url(./src/assets/deportes/sambo.webp)] bg-cover bg-center",
+                        // Preload hover image by setting it in a pseudo-element
+                        "before:bg-[url(./src/assets/deportes/sambogif.webp)] before:fixed before:inset-0 before:opacity-0 before:z-[-1]",
+                        "hover:bg-[url(./src/assets/deportes/sambogif.webp)]",
+                        "hover:after:content-[''] hover:after:absolute hover:after:inset-0 hover:after:bg-black hover:after:opacity-50",
+                        "transition-all duration-500"
+                    )}
+                >
+                    <div className="text relative z-50">
+                        <h1 className="font-bold text-xl md:text-3xl text-gray-50 relative">
+                            Background Overlays
+                        </h1>
+                        <p className="font-normal text-base text-gray-50 relative my-4">
+                            This card is for some special elements, like displaying background
+                            gifs on hover only.
+                        </p>
+                    </div>
+                </div>
             </article>
-            <article className='flex-[1_1_300px] h-[300px]  bg-[url(./src/assets/deportes/thai.jpg)] bg-cover bg-center '>
-             <p className="text">Muay Thai</p> 
+            <article className="flex-[1_1_300px] ">
+                <div
+                    className={cn(
+                        "group w-full cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl mx-auto flex flex-col justify-end p-4 border border-transparent dark:border-neutral-800",
+                        "bg-[url(./src/assets/deportes/thai.jpg)] bg-cover bg-center",
+                        // Preload hover image by setting it in a pseudo-element
+                        "before:bg-[url(./src/assets/deportes/thaigif.webp)] before:fixed before:inset-0 before:opacity-0 before:z-[-1]",
+                        "hover:bg-[url(./src/assets/deportes/thaigif.webp)]",
+                        "hover:after:content-[''] hover:after:absolute hover:after:inset-0 hover:after:bg-black hover:after:opacity-50",
+                        "transition-all duration-500"
+                    )}
+                >
+                    <div className="text relative z-50">
+                        <h1 className="font-bold text-xl md:text-3xl text-gray-50 relative">
+                            Background Overlays
+                        </h1>
+                        <p className="font-normal text-base text-gray-50 relative my-4">
+                            This card is for some special elements, like displaying background
+                            gifs on hover only.
+                        </p>
+                    </div>
+                </div>
+            </article>
+            <article className="flex-[1_1_300px] ">
+                <div
+                    className={cn(
+                        "group w-full cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl mx-auto flex flex-col justify-end p-4 border border-transparent dark:border-neutral-800",
+                        "bg-[url(./src/assets/deportes/taekwondo.avif)] bg-cover bg-center",
+                        // Preload hover image by setting it in a pseudo-element
+                        "before:bg-[url(./src/assets/deportes/taekgif.webp)] before:fixed before:inset-0 before:opacity-0 before:z-[-1]",
+                        "hover:bg-[url(./src/assets/deportes/taekgif.webp)]",
+                        "hover:after:content-[''] hover:after:absolute hover:after:inset-0 hover:after:bg-black hover:after:opacity-50",
+                        "transition-all duration-500"
+                    )}
+                >
+                    <div className="text relative z-50">
+                        <h1 className="font-bold text-xl md:text-3xl text-gray-50 relative">
+                            Background Overlays
+                        </h1>
+                        <p className="font-normal text-base text-gray-50 relative my-4">
+                            This card is for some special elements, like displaying background
+                            gifs on hover only.
+                        </p>
+                    </div>
+                </div>
             </article>
 
-            <article className='flex-[1_1_300px] h-[300px]  bg-[url(./src/assets/deportes/taekwondo.avif)] bg-cover bg-center '>
-             <p className="text">Taekwondo</p>   
-            </article>
-          
+
+
         </>
 
     )
--- frontend/src/components/main/SectionDeportes.jsx
+++ frontend/src/components/main/SectionDeportes.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ArticleDeportes from './ArticleDeportes'
 
+
 const SectionDeportes = () => {
   return (
     <section className='mt-10 flex gap-4 flex-wrap'>
-    
+ 
     <ArticleDeportes></ArticleDeportes>
   </section>
   )
