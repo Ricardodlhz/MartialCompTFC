4d51400c8a3206953e479fe8da3dec3f20c42bed
Ricardo <rihozmar@gmail.com>
2025-05-12 12:13:28 +0200

    cambiar foto de perfil funcionando

--- frontend/src/components/hooks/usePerfil.jsx
+++ frontend/src/components/hooks/usePerfil.jsx
@@ -1,58 +1,79 @@
 import { useState } from "react";
 
 export const usePerfil = () => {
-  const [imagePreview, setImagePreview] = useState(null);
-  const [uploading, setUploading] = useState(false);
-  const [error, setError] = useState(null);
-  const [successMessage, setSuccessMessage] = useState(null);
+    const [imagePreview, setImagePreview] = useState(null);
+    const [uploading, setUploading] = useState(false);
+    const [error, setError] = useState(null);
+    const [successMessage, setSuccessMessage] = useState(null);
 
-  const handleImageChange = (e, userId) => {
-    const file = e.target.files[0];
-    if (file) {
-      setImagePreview(file);
-      uploadImage(file, userId);
-    }
-  };
-
-  const uploadImage = async (file, userId) => {
-    const formData = new FormData();
-    formData.append("imagen", file);
-    formData.append("id_usuario", userId);
-    console.log(file,userId)
-    try {
-      setUploading(true);
-      setError(null);
-      setSuccessMessage(null);
-        
-      const response = await fetch(
-        "http://localhost:5001/api/imagenes", // Cambia a tu endpoint real
-        {
-          method: "POST",
-          body: formData,
+    const handleImageChange = (e, userId) => {
+        const file = e.target.files[0];
+        if (file) {
+            setImagePreview(file);
+            uploadImage(file, userId);
+            borrarImagen(userId)
         }
-      );
+    };
 
-      if (!response.ok) {
-        throw new Error("Error en la subida de la imagen.");
-      }
+    const uploadImage = async (file, userId) => {
+        const formData = new FormData();
+        formData.append("imagen", file);
+        formData.append("id_usuario", userId);
+        console.log(file, userId)
+        try {
+            setUploading(true);
+            setError(null);
+            setSuccessMessage(null);
 
-      const data = await response.json();
-      setSuccessMessage("Imagen subida correctamente 🎉");
-      console.log("Respuesta del servidor:", data);
-    } catch (err) {
-      setError("Hubo un error al subir la imagen.");
-      console.error(err);
-    } finally {
-      setUploading(false);
+            const response = await fetch(
+                "http://localhost:5001/api/imagenes", // Cambia a tu endpoint real
+                {
+                    method: "POST",
+                    body: formData,
+                }
+            );
+
+            if (!response.ok) {
+                throw new Error("Error en la subida de la imagen.");
+            }
+
+            const data = await response.json();
+            
+            setSuccessMessage("Imagen subida correctamente 🎉");
+            console.log("Respuesta del servidor:", data);
+        } catch (err) {
+            setError("Hubo un error al subir la imagen.");
+            console.error(err);
+        } finally {
+            setUploading(false);
+        }
+    };
+
+    const borrarImagen = async (idUsuario) => {
+        const api = await fetch("http://localhost:5001/api/imagenes")
+        const data = await api.json()
+        let id=data.find(dato=>dato.id_usuario==idUsuario)
+         if (data) {
+            const borrarApi = await fetch("http://localhost:5001/api/imagenes/" + id.id, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+
+                }
+            })
+
+            if(borrarApi.ok){
+                location.reload()
+                console.log("BORRADO")
+            }
+        }
+        return data
     }
-  };
-
-  
-  return {
-    imagePreview,
-    uploading,
-    error,
-    successMessage,
-    handleImageChange,
-  };
+    return {
+        imagePreview,
+        uploading,
+        error,
+        successMessage,
+        handleImageChange,
+    };
 };
